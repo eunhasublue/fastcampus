26e7e112e185766bf3cfbb0e7434e70f902ced3d
Lim su hyuck <limsuhyuck@Limui-MacBookPro.local>
2021-06-23 16:43:44 +0900

    공통 모듈 분리

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,20 +1,4 @@
-const searchEL = document.querySelector('.search');
-const searchInputEl = searchEL.querySelector('input');
- 
-searchEL.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEL.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEL.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
+const toTopEl = document.querySelector('#to-top');
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function () {
@@ -44,7 +28,6 @@ window.addEventListener('scroll', _.throttle(function () {
 }, 300));
 // _.throttle(함수, 시간)
 
-const toTopEl = document.querySelector('#to-top');
 toTopEl.addEventListener('click', function(){
   gsap.to(window, .7, {
     scrollTo: 0
@@ -141,6 +124,3 @@ spyEls.forEach(function (spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 올해 년도 출력
